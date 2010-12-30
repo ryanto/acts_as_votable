3c752e1f817c3899ab045af51f9d978351cdc246
Ryan <ryan@ryantoronto.com>
2010-12-30 14:37:38 -0500

    added alias functions

--- lib/acts_as_votable/voter.rb
+++ lib/acts_as_votable/voter.rb
@@ -22,12 +22,14 @@ module ActsAsVotable
         votes = find_votes(:votable_id => votable.id, :votable_type => votable.class.name)
         votes.size > 0
       end
+      alias :voted_for? :voted_on?
 
       def voted_as_when_voting_on votable
         votes = find_votes(:votable_id => votable.id, :votable_type => votable.class.name)
         return nil if votes.size == 0
         return votes.first.vote_flag
       end
+      alias :voted_as_when_voting_for :voted_as_when_voting_on
 
       def find_votes extra_conditions = {}
         ActsAsVotable::Vote.find(:all, :conditions => default_conditions.merge(extra_conditions))
