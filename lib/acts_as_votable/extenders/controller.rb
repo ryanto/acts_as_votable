7dd76fda182f7766ef87c2b4e7fa0500a1cfccc4
Michael Whittaker <post@michael-whittaker.de>
2013-02-25 20:52:28 +0100

    some more stupid voter_params stuff

--- lib/acts_as_votable/extenders/controller.rb
+++ lib/acts_as_votable/extenders/controller.rb
@@ -3,15 +3,15 @@ module ActsAsVotable
 
     module Controller
 
-      def voter_params
-        params.permit(:votable_id, :votable_type,
+      def voter_params(params_object = params[:vote])
+        params_object.permit(:votable_id, :votable_type,
           :voter_id, :voter_type,
           :votable, :voter,
           :vote_flag, :vote_scope)
       end
       
-      def votable_params
-        params.permit(:vote_registered)
+      def votable_params(params_object = params[:vote])
+        params_object.permit(:vote_registered)
       end
       
     end
