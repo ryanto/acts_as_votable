fe8e19392295c031e0fcfc30b9f837ac89dd614d
Eloy <ej.perezgomez@gmail.com>
2017-09-09 13:04:33 +0200

    Transform Klass to String in scopes
    * In Rails 5.1 find by class has been removed

--- lib/acts_as_votable/vote.rb
+++ lib/acts_as_votable/vote.rb
@@ -18,8 +18,8 @@ module ActsAsVotable
 
     scope :up, -> { where(vote_flag: true) }
     scope :down, -> { where(vote_flag: false) }
-    scope :for_type, ->(klass) { where(votable_type: klass) }
-    scope :by_type, ->(klass) { where(voter_type: klass) }
+    scope :for_type, ->(klass) { where(votable_type: klass.to_s) }
+    scope :by_type, ->(klass) { where(voter_type: klass.to_s) }
 
     validates_presence_of :votable_id
     validates_presence_of :voter_id
