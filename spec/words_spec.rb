309ee70a14aab0abdbc38f5179ac8d484640ddf6
Eloy <ej.perezgomez@gmail.com>
2017-09-09 12:34:40 +0200

    Update words_spec to use factories

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -1,32 +1,27 @@
 # frozen_string_literal: true
 
-require "acts_as_votable"
 require "spec_helper"
 
 describe ActsAsVotable::Helpers::Words do
-
-  before :each do
-    @vote = ActsAsVotable::Vote.new
-  end
+  let(:vote) { build(:vote) }
 
   it "should know that like is a true vote" do
-    expect(@vote.votable_words.that_mean_true).to include "like"
+    expect(vote.votable_words.that_mean_true).to include "like"
   end
 
   it "should know that bad is a false vote" do
-    expect(@vote.votable_words.that_mean_false).to include "bad"
+    expect(vote.votable_words.that_mean_false).to include "bad"
   end
 
   it "should be a vote for true when word is good" do
-    expect(@vote.votable_words.meaning_of("good")).to be true
+    expect(vote.votable_words.meaning_of("good")).to be true
   end
 
   it "should be a vote for false when word is down" do
-    expect(@vote.votable_words.meaning_of("down")).to be false
+    expect(vote.votable_words.meaning_of("down")).to be false
   end
 
   it "should be a vote for true when the word is unknown" do
-    expect(@vote.votable_words.meaning_of("lsdhklkadhfs")).to be true
+    expect(vote.votable_words.meaning_of("lsdhklkadhfs")).to be true
   end
-
 end
