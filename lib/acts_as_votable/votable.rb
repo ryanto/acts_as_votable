e771b00e362c5d9fe5b23eb17ab9d375a62cd744
Thomas <thomas@1wingstudio.com>
2017-10-12 11:25:06 -0400

    Add transactions around vote.save and vote.destroy
    Ensures they are also rolled back if update_cached_votes is rolledback,
    for instance by a ActiveRecord::StaleObjectError.

--- lib/acts_as_votable/votable.rb
+++ lib/acts_as_votable/votable.rb
@@ -100,13 +100,15 @@ module ActsAsVotable
       #Allowing for a vote_weight to be associated with every vote. Could change with every voter object
       vote.vote_weight = (options[:vote_weight].to_i if options[:vote_weight].present?) || 1
 
-      if vote.save
-        self.vote_registered = true if last_update != vote.updated_at
-        update_cached_votes options[:vote_scope]
-        return true
-      else
-        self.vote_registered = false
-        return false
+      vote.transaction do
+        if vote.save
+          self.vote_registered = true if last_update != vote.updated_at
+          update_cached_votes options[:vote_scope]
+          return true
+        else
+          self.vote_registered = false
+          return false
+        end
       end
     end
 
@@ -115,8 +117,10 @@ module ActsAsVotable
       votes = find_votes_for(voter_id: args[:voter].id, vote_scope: args[:vote_scope], voter_type: args[:voter].class.base_class.name)
 
       return true if votes.size == 0
-      votes.each(&:destroy)
-      update_cached_votes args[:vote_scope]
+      votes.first.transaction do
+        votes.each(&:destroy)
+        update_cached_votes args[:vote_scope]
+      end
       self.vote_registered = false if votes_for.count == 0
       return true
     end
